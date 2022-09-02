482146dc02507fa7e95387a5206a332b8b5eb4c8
han-hum <hanna.hummal@voco.ee>
2022-09-02 03:17:22 -0700

    First commit, ex5

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String text = "includes text";
+        Integer wholeNumber = 123;
+        Double decimalNumber = 3.14;
+        Boolean isTrue = true;
+        System.out.println("Hello world!");
+    }
+}
